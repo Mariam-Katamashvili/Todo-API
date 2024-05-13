73816da49674d18bb22637ff9f43d7441d5add73
MariamKatamashvili <katamashvili.mariam@kiu.edu.ge>
2024-05-13 21:06:06 +0400

    Cleared master branch

--- src/main/java/com/mariamkatamashvili/todolist/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.mariamkatamashvili.todolist.service;
-
-import com.mariamkatamashvili.todolist.dto.userDto.CreateUserDTO;
-import com.mariamkatamashvili.todolist.dto.userDto.UserDTO;
-
-public interface UserService {
-    UserDTO create (CreateUserDTO createUser);
-}
